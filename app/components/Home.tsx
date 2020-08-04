50a1525cc6571deb704f0cabf059e0e9714a118d
Jiles <jilesdenham@gmail.com>
2020-08-04 22:34:46 +1000

    Changed home component

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -6,7 +6,7 @@ import styles from './Home.css';
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
+      <h2>PLs Change</h2>
       <Link to={routes.COUNTER}>to Counter</Link>
       <Link to={"/test"}>to Test</Link>
     </div>
