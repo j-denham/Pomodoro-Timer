6015bc29ff34629a38b1626da48f165f41a1725e
Jiles <jilesdenham@gmail.com>
2020-08-15 12:30:45 +1000

    Added Timer, re-render bug present

--- pomo-app/src/Pomodoro.tsx
+++ pomo-app/src/Pomodoro.tsx
@@ -11,15 +11,15 @@ type InputProps = {
 }
 
 type TimerProps = {
-  legnth: number;
+  length: number;
   countDown: boolean;
 }
 
 const PomoInput: React.FC<InputProps> = ({ placeholder, setLength }) => {
   return ( 
     <input 
-      placeholder={ placeholder }
-      onChange={ setLength }
+      placeholder={placeholder}
+      onChange={setLength}
     />
   );
 }
@@ -27,52 +27,55 @@ const PomoInput: React.FC<InputProps> = ({ placeholder, setLength }) => {
 const BreakInput: React.FC<InputProps> = ({ placeholder, setLength }) => {
   return (
     <input
-      placeholder={ placeholder }
+      placeholder={placeholder}
       onChange={setLength}
     />
   );
 }
 
 
-const StartButton = () => (
-  <button>
-    LESSSHHHHHGOOOOOO
-  </button>
-);
+//const StartButton = ({setStarted: (e: React.MouseEvent<HTMLButtonElement>) => void}) => (
+//  <button onClick={setStarted}>
+//    LESSSHHHHHGOOOOOO
+//  </button>
+//);
 
-//const Timer: React.FC<TimerProps> = ({pomoLength, isCountingDown}) => {
-//  
-//  const calculateTimeLeft = () = {
-//    let year = new Date().getFullYear();
-//
-//  }
+const Timer: React.FC<TimerProps> = ({length, countDown}) => {  
+  // pomoLength * 60 gives total seconds
+  const [timerLength, setTimerLength] = useState(length*60);
+
+  // setInterval(func, delay). 1000ms = 1 second
+  let timer = setInterval(() => setTimerLength(timerLength - 1),  1000);
   
+  return (
+    <div>
+      {Math.floor(timerLength / 60)}:{timerLength % 60}
+    </div>
+  );
+}; 
 
-
-//}; 
-
-const Pomodoro = ({}) => {
+const Pomodoro = () => {
   
   const [pomoLength, setPomoLength] = useState(0);
   const [breakLength, setBreakLength] = useState(0);
   const [blockLength, setBlockLength] = useState(0);
   const [isCountingDown, setIsCountingDown] = useState(true);
+  const [started, setStarted] = useState(false);
 
   return (
     <div>
       <PomoInput setLength={(e) => setPomoLength(parseInt(e.target.value))} placeholder='Pomodoro Length'/>
       <BreakInput setLength={(e) => setBreakLength(parseInt(e.target.value))} placeholder='Break Length'/>
-      <StartButton/>
       <p className='yudodis'>Pomo Length: {pomoLength}</p>
+      <button onClick={() => setStarted(true)}>
+        LESSSHHHHHGOOOOOO
+      </button>
       <p className='cooked'>Break Length: {breakLength}</p>
-    {/* <Timer length={pomoLength} countDown={isCountingDown}/> */}
+      {started ? (
+        <Timer length={pomoLength} countDown={isCountingDown}/>
+      ) : null}
     </div>
   );
 } 
 
 export default Pomodoro
-// Flexbox
-// grid
-// margin
-// Padding
-// font stuff
